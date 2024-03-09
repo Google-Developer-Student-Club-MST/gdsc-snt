6fdbd06f2e086537701dc56adc9bfc3cc75566ed
tkbessler <tkbessler@gmail.com>
2024-03-09 13:31:53 -0600

    created route to about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'about', component: AboutComponent },
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SolidButtonComponent } from '../solid-button/solid-button.component';
 import { HollowButtonComponent } from '../hollow-button/hollow-button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SolidButtonComponent, HollowButtonComponent],
+  imports: [SolidButtonComponent, HollowButtonComponent, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })

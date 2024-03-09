a6613d43181a282ad681c45eddcadfedf5c4a4fc
tkbessler <tkbessler@gmail.com>
2024-03-09 14:04:40 -0600

    basic join-today structure

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HollowButtonComponent } from '../hollow-button/hollow-button.component';
+import { SolidButtonComponent } from '../solid-button/solid-button.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [SolidButtonComponent, HollowButtonComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })

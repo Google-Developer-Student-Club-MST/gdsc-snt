100586101f711405c08fb872a09e34e6468d70a0
dheeraj9489 <dheerajmb19@gmail.com>
2024-03-28 12:39:37 -0500

    completed green club

--- src/app/components/solid-button/solid-button.component.ts
+++ src/app/components/solid-button/solid-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-solid-button',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './solid-button.component.scss'
 })
 export class SolidButtonComponent {
-
+  @Input() customClass!: string;
 }
